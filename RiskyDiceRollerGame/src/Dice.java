aa46a0a60d814d6430978c6ee0ecd2ca1480ee0e
Samuel Horner <Samuelhorner123@gmail.com>
2025-08-25 18:03:01 -0400

    Added a combat result helper

--- RiskyDiceRollerGame/src/Dice.java
+++ RiskyDiceRollerGame/src/Dice.java
@@ -6,14 +6,18 @@ public class Dice {
     private int result;
 
     /*
-        rolls a die using numSides and random, the result is the side of the dice that was rolled
-     */ 
+    rolls a die using numSides and random, the result is the side of the dice that was rolled
+    */ 
     public int rollDice(int numSides) {
         Random random = new Random();
         int roll = random.nextInt(numSides)+1; // +1 prevents 0 result    
         return roll;
     }
 
+    //checks if the player rolled between the ranges accounting for the defense buffer
+    public boolean combatResult(int defenseTotal, int attackTotal, int minRange, int maxRange){
+        return (attackTotal > (minRange - defenseTotal) || attackTotal < (maxRange + defenseTotal));
+    }
 
 
 }
