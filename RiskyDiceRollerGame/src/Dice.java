cc7e70fcb3f7a351d7958e1182e3d9b47306668b
Samuel Horner <Samuelhorner123@gmail.com>
2025-08-25 17:36:17 -0400

    Created Dice class

---
+++ RiskyDiceRollerGame/src/Dice.java
@@ -0,0 +1,17 @@
+import java.security.*;
+
+public class Dice {
+    private boolean rolled;
+    private int numSides;
+    private int result;
+    private SecureRandom random;
+
+    private int rollDice(int type) {
+        switch(type){
+            case 0:
+            break;
+        }
+        
+        return 0;
+    }
+}
