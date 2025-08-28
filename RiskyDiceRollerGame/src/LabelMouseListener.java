1d0b5744533ea02f3b0a81b2c42d57c632899c54
FabeDaBabe <fabia@Fabians-PC>
2025-08-28 04:48:02 -0400

    Finally made probably 90% of code required for main map

---
+++ RiskyDiceRollerGame/src/LabelMouseListener.java
@@ -0,0 +1,68 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+
+public class LabelMouseListener extends MouseAdapter {
+
+	private Gooey view;
+
+	/**
+	 * The JLabel representing the card
+	 */
+	private JLabel label;
+
+	/**
+	 * Reference to the Controller for game actions
+	 */
+	private Controller controller;
+	
+	/**
+	 * Constructor to initialize the LabelMouseListener.
+	 * 
+	 * @param view The game view.
+	 * @param controller The game controller.
+	 * @param label The JLabel associated with the card.
+	 */
+	LabelMouseListener(Gooey view, Controller controller, JLabel label){
+		this.view = view;
+		this.controller = controller;
+		this.label = label;
+	}
+
+	/**
+	 * Handles mouse hover over a card, displaying the full card image.
+	 * 
+	 * @param e The mouse event triggered when hovering over the card.
+	 */
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		//TODO what happens when you hover over a level
+	}
+
+	/**
+	 * Handles when the mouse leaves the card, restoring its previous display mode.
+	 * 
+	 * @param e The mouse event triggered when leaving the card.
+	 */
+	@Override
+	public void mouseExited(MouseEvent e) {
+		//TODO what happens when your mouse exits a level
+	}
+
+	/**
+	 * Handles card selection when clicked, updating the UI and notifying the controller.
+	 * 
+	 * @param e The mouse event triggered when the card is clicked.
+	 */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//TODO what happens when you click on a level
+	}
+	
+	
+	
+}
+
+
+
